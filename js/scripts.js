c438f62f0e40d2bb860b35e82df7589002b66dd6
AJ Mcgladdery and Colin Cook <aj@>
2018-08-15 10:24:55 -0700

    Add front-end logic to show results.

--- js/scripts.js
+++ js/scripts.js
@@ -9,18 +9,6 @@ var leapYear = function(year) {
 };
 
 
-
-
-
-
-
-
-
-
-
-
-
-
 //Front-End Logic
 
 $(document).ready(function() {
@@ -28,6 +16,16 @@ $(document).ready(function() {
     event.preventDefault();
     var year = parseInt($("input#year").val());
     var result = leapYear(year);
-    $("#result").text(result);
+
+    $(".year").text(year);
+
+    if (!result) {
+      $(".not").text("not");
+    } else {
+      $(".not").text("");
+    }
+
+    $("#result").show();
+
   });
 });
