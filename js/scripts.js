17488ec4dccbb6be8b306c7328bb2538b547c68d
AJ Mcgladdery and Colin Cook <aj@>
2018-08-15 08:30:51 -0700

    Add two specs to leapYear function.

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+//Back-End Logic
+
+var leapYear = function(year) {
+  if (year % 4 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Front-End Logic
+
+$(document).ready(function() {
+  $("form#leap-year").submit(function(event) {
+    event.preventDefault();
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+    $("#result").text(result);
+  });
+});
